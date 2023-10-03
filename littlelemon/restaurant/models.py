5ff66a0934c94f64f83a36c9ae62940b066b0e5e
tahagkcgz <gokcegoz.taha@metu.edu.tr>
2023-10-04 00:32:03 +0300

    Created API

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,16 +1,13 @@
 from django.db import models
-from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 # Create your models here.
 class Menu(models.Model):
-    id = models.IntegerField(primary_key=True, validators=[MaxLengthValidator(11),MinLengthValidator(11)], auto_created=True)
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.IntegerField(validators=[MaxLengthValidator(5),MinLengthValidator(1)])
+    inventory = models.IntegerField()
 
 
 class Booking(models.Model):
-    id = models.IntegerField(primary_key=True, validators=[MaxLengthValidator(5),MinLengthValidator(5)], auto_created=True)
     name = models.CharField(max_length=255)
-    no_of_guests = models.IntegerField(validators=[MaxLengthValidator(6),MinLengthValidator(1)])
-    booking_date = models.DateTimeField(auto_now=True)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+class MenuView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
